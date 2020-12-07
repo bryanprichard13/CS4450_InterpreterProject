eee592e0313d68d54037a001a9932ad8e6ee7414
Micah <motc34@umsystem.edu>
2020-12-07 11:39:11 -0600

    Added arithmetic operators

---
+++ number.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+//#include "exception.h"
+#include "number.h"
+
+using std::ostream;
+using std::fixed;
+
+Number Number::operator+(Number& number) {
+    switch(type){
+        case (INT):
+            if (number.type == INT) { return Number(value.i + number.value.i); }
+            else { return Number(value.i + number.value.d); }
+            break;
+        case (FLOAT):
+            if (number.type == FLOAT) { return Number(value.d + number.value.d); }
+            else {return Number(value.d + number.value.i);  }
+            break;
+        default:
+            //throw UnknownType();
+    }
+}
+
+Number Number::operator-(Number& number) {
+    switch(type){
+        case (INT):
+            if (number.type == INT) { return Number(value.i - number.value.i); }
+            else { return Number(value.i - number.value.d); }
+            break;
+        case (FLOAT):
+            if (number.type == FLOAT) { return Number(value.d - number.value.d); }
+            else {return Number(value.d - number.value.i);  }
+            break;
+        default:
+            //throw UnknownType();
+    }
+}
+
+Number Number::operator*(Number& number) {
+    switch(type){
+        case (INT):
+            if (number.type == INT) { return Number(value.i * number.value.i); }
+            else { return Number(value.i * number.value.d); }
+            break;
+        case (FLOAT):
+            if (number.type == FLOAT) { return Number(value.d * number.value.d); }
+            else {return Number(value.d * number.value.i);  }
+            break;
+        default:
+            //throw UnknownType();
+    }
+}
+
+Number Number::operator/(Number& number) {
+    switch(type){
+        case (INT):
+            if (number.type == INT) { return Number(value.i / number.value.i); }
+            else { return Number(value.i / number.value.d); }
+            break;
+        case (FLOAT):
+            if (number.type == FLOAT) { return Number(value.d / number.value.d); }
+            else {return Number(value.d / number.value.i);  }
+            break;
+        default:
+            //throw UnknownType();
+    }
+}
+
+Number Number::operator%(Number& number) {
+    switch(type){
+        case (INT):
+            if (number.type == INT) { 
+                if (number.value.i == 0) { /*throw ModByZero();*/}
+                return Number(value.i % number.value.i); 
+                }
+            else { /*throw FloatingPointMod(); */}
+            break;
+        case (FLOAT):
+            //throw FloatingPointMod();
+            break;
+        default:
+            //throw UnknownType();
+    }
+}
---
+++ number.h
@@ -0,0 +1,56 @@
+#ifndef NUMBER_H
+#define NUMBER_H
+
+#include <iosfwd>
+
+using std::ostream;
+
+typedef enum Type {
+   INT,
+   FLOAT
+} Type;
+
+typedef union Value {
+   Value(int    _int) : i(_int) { }
+   Value(double _double) : d(_double) { }
+
+   int    i;
+   double d;
+} Value;
+
+class Number {
+public:
+   Number()                   : type(INT),   value(0)     { }
+   Number(const int    input) : type(INT),   value(input) { }
+   Number(const double input) : type(FLOAT), value(input) { }
+
+   Number  operator+  (Number&);
+   Number  operator-  (Number&);
+   Number  operator*  (Number&);
+   Number  operator/  (Number&);
+   Number  operator%  (Number&);
+   Number  operator^  (Number&);
+
+   Number& operator=  (Number&);
+   Number& operator+= (Number&);
+   Number& operator-= (Number&);
+   Number& operator*= (Number&);
+   Number& operator/= (Number&);
+   Number& operator^= (Number&);
+   Number& operator%= (Number&);
+
+   friend bool     operator<  (Number&, Number&);
+   friend bool     operator<= (Number&, Number&);
+   friend bool     operator>  (Number&, Number&);
+   friend bool     operator>= (Number&, Number&);
+   friend bool     operator== (Number&, Number&);
+   friend bool     operator!= (Number&, Number&);
+
+   
+
+private:
+   Type  type;
+   Value value;
+};
+
+#endif
