d7f2d954bd84d69da8bd7d242178c10c02f19453
ChristianCaldwell55 <christian.c2969@gmail.com>
2020-12-16 14:50:28 -0600

    Push up for loop

--- statement.h
+++ statement.h
@@ -15,9 +15,8 @@ class IfElse : public Statement {
 public:
    IfElse(Expression *cond, StatementList* tList, StatementList* fList) : condition(cond), trueList(tList), falseList(fList) { }
    IfElse(Expression *cond, StatementList* tList) : condition(cond), trueList(tList), falseList(new StatementList()) { }
-   ~IfElse();
 
-   void check(SymbolTable&, FunctionTable&) const;
+   void check(SymbolTable&) const;
 
 private:
    Expression*    condition;
@@ -29,31 +28,28 @@ private:
 class While : public Statement {
 public:
    While(Expression* cond, StatementList* sList) : condition(cond), stmtList(sList) { }
-   ~While();
 
-   void check(SymbolTable&, FunctionTable&) const;
+   void check(SymbolTable&) const;
 
 private:
    Expression*    condition;
    StatementList* stmtList;
 };
 
+class For : public Statement {
+public:
+   For(Expression* cond, Expression* state, Expression* action StatementList* sList) : condition(cond), condition(state), condition(action), stmtList(sList) { }
+
+   void check(SymbolTable&) const;
+
+private:
+   Expression*    condition;
+   StatementList* stmtList;
+}
 
 class Print : public Statement {
 public:
    Print(Expression* e) : exp(e) { }
-   ~Print();
-
-   void check(SymbolTable&, FunctionTable&) const;
-
-private:
-   Expression* exp;
-};
-
-class Comment : public Statement {
-public:
-   Comment(Expression* e) : exp(e) { }
-   ~Comment();
 
    void check(SymbolTable&, FunctionTable&) const;
 
