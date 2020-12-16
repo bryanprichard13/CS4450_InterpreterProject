46446514500d5f914b7d642a8057a96c7c925f6b
bryanprichard13 <bryanprichard13@gmail.com>
2020-12-16 10:53:22 -0600

    conditional.cpp functiontable removal

--- conditional.cpp
+++ conditional.cpp
@@ -1,26 +1,26 @@
 #include "symbolTable.h"
 #include "conditional.h"
 
-Number LessThan::check(const SymbolTable& st, const FunctionTable& ft) const {
-   return op1->check(st, ft) < op2->check(st, ft);
+Number LessThan::check(const SymbolTable& st) const {
+   return op1->check(st) < op2->check(st);
 }
 
-Number GreaterThan::check(const SymbolTable& st, const FunctionTable& ft) const {
-   return op1->check(st, ft) > op2->check(st, ft);
+Number GreaterThan::check(const SymbolTable& st) const {
+   return op1->check(st) > op2->check(st);
 }
 
-Number LessThanOrEqualTo::check(const SymbolTable& st, const FunctionTable& ft) const {
-   return op1->check(st, ft) <= op2->check(st, ft);
+Number LessThanOrEqualTo::check(const SymbolTable& st) const {
+   return op1->check(st) <= op2->check(st);
 }
 
-Number GreaterThanOrEqualTo::check(const SymbolTable& st, const FunctionTable& ft) const {
-   return op1->check(st, ft) >= op2->check(st, ft);
+Number GreaterThanOrEqualTo::check(const SymbolTable& st) const {
+   return op1->check(st) >= op2->check(st);
 }
 
-Number Equals::check(const SymbolTable& st, const FunctionTable& ft) const {
-   return op1->check(st, ft) == op2->check(st, ft);
+Number Equals::check(const SymbolTable& st) const {
+   return op1->check(st) == op2->check(st);
 }
 
-Number NotEquals::check(const SymbolTable& st, const FunctionTable& ft) const {
-   return op1->check(st, ft) != op2->check(st, ft);
+Number NotEquals::check(const SymbolTable& st) const {
+   return op1->check(st) != op2->check(st);
 }
